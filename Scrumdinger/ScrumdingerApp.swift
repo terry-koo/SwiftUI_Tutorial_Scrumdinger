40975c02a52e76abde001e96dbcdaa9e6c12af37
terry-koo <xpflxhfl94@naver.com>
2022-05-01 16:08:01 +0900

    create Scrumdinger project

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Terry Koo on 2022/05/01.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
